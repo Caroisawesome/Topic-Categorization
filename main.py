9893e728f14683cb360a2857d780c711090de269
Keira Haskins <keirahaskins26@gmail.com>
2020-03-03 15:15:25 -0700

    Added pickle load to main.py.

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import pickle
-import util
+from util import Sparse_CSR
 
 
 if (__name__ == '__main__'):
     file = open('sparse_testing', 'rb')
     matrix = pickle.load(file)
     file.close()
-    val = matrix.access_element(6, 10)
+    val = matrix.access_element(7, 10)
     print('Value is: ' + str(val))
 
